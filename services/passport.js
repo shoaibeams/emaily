058e8f5cdcf7e1ce03e0f60f37317dcd4c928b54
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-03-06 23:28:26 +0500

    Added passport auth

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
+//creating a user token with user id, not profile id
+passport.serializeUser((user, done) => {
+  done(null, user.id) //id === _id
+})
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id)
+  done(null, user)
+})
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id })
+      if (existingUser) {
+        //we already have a user
+        done(null, existingUser)
+      } else {
+        const user = await new User({ googleID: profile.id }).save()
+        done(null, user)
+      }
+    }
+  )
+)
