20cf65b14f1c2072352ca57d0f9632a37e6d6cda
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-03-09 11:26:39 +0500

    Added scripts pertaining to deployment to heroku

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up prod assets
+  //like our main.js file or main.css file
+  app.use(express.static('client/build'))
+
+  //Express will serve up the index.html file
+  //if it doesn't recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  }) // if nothing matches return index.html
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
