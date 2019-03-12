8515104f1edabacb2c7ca5fd0756a7d0be075260
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-03-12 15:55:33 +0500

    Final commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./models/User')
 require('./models/Survey')
 require('./services/passport')
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
 const app = express()
 
 app.use(bodyParser.json())
