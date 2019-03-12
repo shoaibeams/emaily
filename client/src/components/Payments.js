a81b33658be97c27d27ba8f9b14974c99155c07e
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-03-12 18:12:47 +0500

    Tweaked stipe key

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,14 +5,13 @@ import * as actions from '../actions'
 
 class Payments extends Component {
   render() {
-    console.log(process.env.REACT_APP_STRIPE_KEY)
     return (
       <StripeCheckout
         name="Emaily"
         description="$5 for 5 email credits"
         amount={500} //100 equals 1USD
         token={token => this.props.handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        stripeKey="pk_test_k1djvF6BxanOYiBkg1oRhOdY"
       >
         <button className="btn">Add credit</button>
       </StripeCheckout>
